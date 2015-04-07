9139f0fce1cf5794fbd44579dc2358423d2deb8f
CarlosGRodrigzeL <crolo@gmx.at>
2015-04-07 22:59:21 +0200

    Controller Oberklasse, IPatientController, PatientController
    Controller - Oberklasse mit statischem Session-Objekt
    IPatientController - Interface mit erster Methode
    PatientController - createNewPatient implementiert

---
+++ src/at/itb13/oculus/application/Controller.java
@@ -0,0 +1,9 @@
+package at.itb13.oculus.application;
+
+import at.itb13.oculus.database.DBFacade;
+
+public class Controller {
+	protected static final DBFacade _database  = new DBFacade();
+	
+	
+}
