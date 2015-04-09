b5de4fe7cfdb288b8d4f02967f0fd51e8a81e315
DomKB <dominik.blaser@students.fhv.at>
2015-04-09 17:47:33 +0200

    Add Main for starting GUI

---
+++ test/at/itb13/oculus/test/MainPresentation.java
@@ -0,0 +1,17 @@
+package at.itb13.oculus.test;
+
+import at.itb13.oculus.presentation.GUIApplication;
+
+public class MainPresentation {
+
+	
+	public static void main (String [] args){
+			
+		GUIApplication.main(args);
+		
+		//launch(args);
+		
+		
+		
+	}
+}
