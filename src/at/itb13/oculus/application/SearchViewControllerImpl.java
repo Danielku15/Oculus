3371ae3388df1e646ef233b57f86d6f4b77b6294
Patrick Schedler <patrick_schedler@hotmail.com>
2015-04-10 09:53:48 +0200

    changed index service to timer task

--- src/at/itb13/oculus/application/SearchViewControllerImpl.java
+++ src/at/itb13/oculus/application/SearchViewControllerImpl.java
@@ -1,14 +1,5 @@
 package at.itb13.oculus.application;
 
-import java.util.List;
-
-import org.hibernate.search.FullTextSession;
-import org.hibernate.search.Search;
-
 public class SearchViewControllerImpl<T> extends Controller implements SearchViewController {
-	
-	public List<T> search(String criteria) {
-		
-	}
 
 }
