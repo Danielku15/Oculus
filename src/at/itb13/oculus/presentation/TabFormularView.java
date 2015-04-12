997692dcae54b0a21fcbbdef9604b49465e5cca6
DomKB <dominik.blaser@students.fhv.at>
2015-04-12 11:46:41 +0200

    include patient search view

--- src/at/itb13/oculus/presentation/TabFormularView.java
+++ src/at/itb13/oculus/presentation/TabFormularView.java
@@ -6,22 +6,53 @@ import at.itb13.oculus.lang.LangFacade;
 import at.itb13.oculus.lang.LangKey;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.control.TabPane;
 import javafx.scene.control.Tab;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
 public class TabFormularView {
+	
+	public static final String FORMULARVIEWXML = "Formular.fxml";
+	public static final String PATIENTSEARCHVIEW = "PatientSearchView.fxml";
+	
 	@FXML
 	private Button _createTabButton;
 	@FXML
 	private TabPane _tabPane;
 	@FXML
 	private Tab _newPatient;
+	@FXML
+    private Button _searchPatient;
+    @FXML
+    private VBox _vBox;
+	
+    // open new window with patient search
+    @FXML
+    void searchPatient(ActionEvent event) {
+    	LangFacade facade = LangFacade.getInstance();
+		
+    	Stage stage = new Stage();
+		Pane pane = null;
+		
+	        try {
+	            pane = FXMLLoader.<Pane>load(this.getClass().getResource(PATIENTSEARCHVIEW),facade.getResourceBundle());
+	        } catch (IOException ex) {
+	            ex.printStackTrace();
+	        }
+	     
+         stage.setScene(new Scene(pane));  
+         stage.show();
+    }
 
+    // creates new tab with formular included
 	// Event Listener on Button[#_createTabButton].onAction
 	@FXML
 	public void createNewTab(ActionEvent event) {
@@ -29,7 +60,7 @@ public class TabFormularView {
 		 
 		 Pane pane = null;
 	        try {
-	            pane = FXMLLoader.<Pane>load(this.getClass().getResource("Formular.fxml"),facade.getResourceBundle());
+	            pane = FXMLLoader.<Pane>load(this.getClass().getResource(FORMULARVIEWXML),facade.getResourceBundle());
 	        } catch (IOException ex) {
 	            ex.printStackTrace();
 	        }
