c05d37e9bbbec6d9d142006268b04270e709d0a1
CarlosGRodrigzeL <crolo@gmx.at>
2015-04-14 08:49:01 +0200

    abstände :P

--- src/at/itb13/oculus/application/ReceivePatientController.java
+++ src/at/itb13/oculus/application/ReceivePatientController.java
@@ -5,6 +5,9 @@ import at.itb13.oculus.model.Patient;
 public interface ReceivePatientController {
 
 	public Patient nextPatient();
+	
 	public Patient showNextPatient();
+	
 	public void newUntersuchungsprotokoll();
+	
 }
