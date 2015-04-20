47c492c5c343354f3ab2e26164f77b67f42a0116
CarlosGRodrigzeL <crolo@gmx.at>
2015-04-20 12:08:18 +0200

    added addbutton in queueview

--- src/at/itb13/oculus/presentation/QueueViewController.java
+++ src/at/itb13/oculus/presentation/QueueViewController.java
@@ -1,5 +1,6 @@
 package at.itb13.oculus.presentation;
 
+import java.io.IOException;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
@@ -12,16 +13,20 @@ import at.itb13.oculus.application.QueueEntryControllerImpl;
 import at.itb13.oculus.lang.LangFacade;
 import at.itb13.oculus.lang.LangKey;
 import at.itb13.oculus.model.Employee;
+import at.itb13.oculus.model.Patient;
 import at.itb13.oculus.model.Queue;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
 
 /**
@@ -34,6 +39,7 @@ public class QueueViewController implements Serializable, Consumer<Boolean>{
 	private QueueControllerImpl _queueController;
 	private QueueEntryControllerImpl _queueEntryController;
 	public static final String QUEUEENTRYVIEW = "QueueEntryView.fxml";
+	private Stage _queueEntryViewStage;
 	private List<String[]> _queues;
 	private List<String[]> _employees;
 	
@@ -84,20 +90,37 @@ public class QueueViewController implements Serializable, Consumer<Boolean>{
 		
 	}
 	
-	public void loadQueueEntryView(){
+	@FXML
+	public void loadQueueEntryView(){		
+		_queueEntryViewStage = new Stage();
 		FXMLLoader loader = null;
+		Pane pane = null;
 		LangFacade facade = LangFacade.getInstance();
+
 		loader = new FXMLLoader(this.getClass().getResource(QUEUEENTRYVIEW),
 				facade.getResourceBundle());
+		try {
+			pane = loader.load();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		_queueEntryViewStage.setScene(new Scene(pane));
+		_queueEntryViewStage.show();
 	}
 	
 	public void refresh(){
 		//TODO implement method
+		String queueId = _queueController.getIdOfQueue(_queueViewEmployeeSelection.getSelectionModel().getSelectedItem());
+		setListView(_queueController.getQueueEntries(queueId));
 	}
 	
 	@Override
 	public void accept(Boolean b) {
 		// TODO something with consumer from QueueEntryViewController
-		
+		if(b){
+			refresh();
+		}
+		_queueEntryViewStage.close();
 	}
 }
