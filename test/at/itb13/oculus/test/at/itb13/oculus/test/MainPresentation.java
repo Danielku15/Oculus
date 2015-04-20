981eff04306fed9733ff4705122f4400e98e14b2
ManuelaLjubicic <manuela.ljubicic@students.fhv.at>
2015-04-20 09:38:43 +0200

    QueueEntryView updated

--- test/at/itb13/oculus/test/at/itb13/oculus/test/MainPresentation.java
+++ test/at/itb13/oculus/test/at/itb13/oculus/test/MainPresentation.java
@@ -1,13 +1,13 @@
-package at.itb13.oculus.test;
-
-import at.itb13.oculus.presentation.GUIApplication;
-
-public class MainPresentation {
-	
-	public static void main (String [] args){
-			
-		GUIApplication.main(args);
-		
-		//launch(args);
-	}
-}
+////package at.itb13.oculus.test;
+//
+//import at.itb13.oculus.presentation.GUIApplication;
+//
+//public class MainPresentation {
+//	
+//	public static void main (String [] args){
+//			
+//		GUIApplication.main(args);
+//		
+//		//launch(args);
+//	}
+//}
