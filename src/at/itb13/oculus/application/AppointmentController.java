067955c8e8f014e5dd27b9c8e14af73c2067f9bd
ManuelaLjubicic <manuela.ljubicic@students.fhv.at>
2015-04-22 11:19:31 +0200

    appointment controller, class + interface created

---
+++ src/at/itb13/oculus/application/AppointmentController.java
@@ -0,0 +1,14 @@
+/**
+ * Autor: Manu Ljubicic
+ * Projekt: Oculus
+ * Datum: 22.04.2015
+ */
+package at.itb13.oculus.application;
+
+/**
+ * @author Manu
+ *
+ */
+public interface AppointmentController extends AutoCloseable{
+
+}
---
+++ src/at/itb13/oculus/application/AppointmentControllerImpl.java
@@ -0,0 +1,14 @@
+/**
+ * Autor: Manu Ljubicic
+ * Projekt: Oculus
+ * Datum: 22.04.2015
+ */
+package at.itb13.oculus.application;
+
+/**
+ * @author Manu
+ *
+ */
+public class AppointmentControllerImpl extends Controller implements AppointmentController{
+
+}
