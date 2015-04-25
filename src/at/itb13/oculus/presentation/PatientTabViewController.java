194d55f670ce27763bc252e4893639c4ca6d8dd7
DomKB <dominik.blaser@students.fhv.at>
2015-04-25 17:28:15 +0200

    change method name

--- src/at/itb13/oculus/presentation/PatientTabViewController.java
+++ src/at/itb13/oculus/presentation/PatientTabViewController.java
@@ -87,7 +87,7 @@ public class PatientTabViewController implements Initializable {
 		patientSearchViewController.addConsumer(new Consumer<String>() {
 			@Override
 			public void accept(String id) {
-				createFormular(id);
+				createForm(id);
 			}
 		});
 		
@@ -149,7 +149,7 @@ public class PatientTabViewController implements Initializable {
 
 	}
 	
-	public void createFormular(String id){
+	public void createForm(String id){
 		LangFacade facade = LangFacade.getInstance();
 		FXMLLoader loader = null;
 		Pane pane = null;
